ef75134c29cdab2a4938bd0b665176aa96fccda5
wolone <71954466@qq.com>
2022-11-29 22:48:16 +0800

    初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+    themeConfig: {
+        siteTitle: 'WOWFANS',
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '数据库', link: '/sql' },
+        { text: 'Changelog', link: 'https://github.com/...' }
+      ]
+    }
+  }  
